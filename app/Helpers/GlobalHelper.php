85370be7030bdbfa06c255eab7f09dc673c7f5af
jovinull <felipejovinogamerplay@gmail.com>
2026-01-02 16:44:20 -0300

    feat: novos helpers globais para sidebar active do admin e user, getWishList e o Global Auth Check

--- app/Helpers/GlobalHelper.php
+++ app/Helpers/GlobalHelper.php
@@ -2,6 +2,7 @@
 
 use App\Models\Category;
 use App\Models\User;
+use App\Models\Wishlist;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -18,19 +19,7 @@ if (!function_exists('isApprovedUser')) {
     }
 }
 
-/** set admin sidebar active */
 
-if (!function_exists('setSidebar')) {
-    function setSidebar(array $routes): ?string
-    {
-        foreach ($routes as $route) {
-            if (request()->routeIs($route)) {
-                return 'mm-active';
-            }
-        }
-        return null;
-    }
-}
 
 /* Global Use in category  */
 
@@ -48,4 +37,63 @@ if (!function_exists('getCourseCategories')) {
     {
         return Category::with('course', 'course.user', 'course.course_goal')->get();
     }
+}
+
+
+/** set admin sidebar active */
+
+if (!function_exists('setSidebar')) {
+    function setSidebar(array $routes): ?string
+    {
+        foreach ($routes as $route) {
+            if (request()->routeIs($route)) {
+                return 'mm-active';
+            }
+        }
+        return null;
+    }
+}
+
+
+/** for user Dashboard only */
+if (!function_exists('setSidebarActive')) {
+    function setSidebarActive(array $routes): ?string
+    {
+        foreach ($routes as $route) {
+            if (request()->routeIs($route)) {
+                return 'page-active';
+            }
+        }
+        return null;
+    }
+}
+
+
+/*  wishlist data */
+if (!function_exists('getWishlist')) {
+    function getWishlist()
+    {
+
+        // Check if user is authenticated
+        if (Auth::check()) {
+            $user_id = Auth::user()->id;
+            return Wishlist::where('user_id', $user_id)->with('course', 'course.user')->get();
+        }
+        // If user is not logged in, return an empty collection or handle as needed
+        return collect();
+    }
+}
+
+
+//Global Auth Check
+
+function auth_check_json()
+{
+    if (!Auth::check()) {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'You must be logged in to perform this action.',
+        ], 401); // 401 Unauthorized
+    }
+    return null;
 }
