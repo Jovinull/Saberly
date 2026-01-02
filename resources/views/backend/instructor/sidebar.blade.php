7a6198aebd664fbb2dd6fa540f549e016e25ef17
jovinull <felipejovinogamerplay@gmail.com>
2026-01-02 17:24:18 -0300

    fix: removendo aba não usada e Z solto

--- resources/views/backend/instructor/sidebar.blade.php
+++ resources/views/backend/instructor/sidebar.blade.php
@@ -19,42 +19,6 @@
             </a>
 
         </li>
-
-        @if(isApprovedUser())
-
-
-
-            <li class="{{ setSidebar(['instructor.course*', 'instructor.course-section*']) }}">
-                <a href="javascript:;" class="has-arrow">
-                    <div class="parent-icon"><i class="bx bx-category"></i>
-                    </div>
-                    <div class="menu-title">Manage Courses</div>
-                </a>
-                <ul>
-                    <li class="{{ setSidebar(['instructor.course*', 'instructor.course-section']) }}">
-                        <a href="{{route('instructor.course.index')}}"><i class='bx bx-radio-circle'></i>All Course</a>
-                    </li>
-
-                </ul>
-            </li>
-
-            <li class="{{ setSidebar(['instructor.coupon*']) }}">
-                <a href="javascript:;" class="has-arrow">
-                    <div class="parent-icon"><i class="bx bx-category"></i>
-                    </div>
-                    <div class="menu-title">Managed Coupon</div>
-                </a>
-                <ul>
-                    <li class="{{ setSidebar(['instructor.coupon*']) }}">
-                        <a href="{{route('instructor.coupon.index')}}"><i class='bx bx-radio-circle'></i>All Coupon</a>
-                    </li>
-
-                </ul>
-            </li>
-
-        @endif
-
-
     </ul>
     <!--end navigation-->
-</div>z
+</div>
